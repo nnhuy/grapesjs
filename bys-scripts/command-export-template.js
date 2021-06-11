1f39879590ace65b1aa155952827582c5986a9ef
DESKTOP-LS4G070\huyth <huy.nguyen.nhat@bys.vn>
2021-06-11 15:57:12 +0700

    - update code

--- bys-scripts/command-export-template.js
+++ bys-scripts/command-export-template.js
@@ -28,39 +28,16 @@ export default grapesjs.plugins.add('bys-command-custom-export-template', (edito
                 .once('change:open', () => editor.stopCommand(this.id));
             const htmlElement = document.createElement('html');
             const headElement = document.createElement('head');
-            const bootstrapCssElement = document.createElement('link');
-            bootstrapCssElement.href = `https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css`;
-            bootstrapCssElement.crossOrigin = `anonymous`;
-            bootstrapCssElement.rel = `stylesheet`;
-            bootstrapCssElement.integrity = `sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x`;
-            headElement.append(bootstrapCssElement);
-            const bootstrapScriptElement = document.createElement('script');
-            bootstrapScriptElement.src = `https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js`;
-            bootstrapScriptElement.integrity = `sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4`;
-            bootstrapScriptElement.crossOrigin = `anonymous`;
-            headElement.append(bootstrapScriptElement);
+            const defaultStyle = createDefaultStyle();
+            headElement.append(defaultStyle);
             const bodyElement = document.createElement('body');
             const allElement = document.createElement('div');
             allElement.innerHTML = editor.getHtml();
             const allElementHasOnLoad = allElement.querySelectorAll('[bys-event="onload"]') || [];
             const scriptTag = document.createElement('script');
             scriptTag.append('\n');
-            scriptTag.append(`let sendEmailModal;\n
-            function sendEmail() {
-                const formSendEmail = document.getElementById('formSendEmail');
-                const formSendEmailData = new FormData(formSendEmail);
-                for (var pair of formSendEmailData.entries()) {
-                    console.log(pair[0] + ', ' + pair[1]);
-                }
-                sendEmailModal.hide();
-                formSendEmail.reset();
-            }\n
-            function openModalSendEmail() {
-                sendEmailModal.show();
-            }\n
-            `);
             scriptTag.append("window.addEventListener('DOMContentLoaded', function() {\n");
-            scriptTag.append(`sendEmailModal = new bootstrap.Modal(document.getElementById('sendEmailModal'), {});\n`);
+            /* build onload script with all div and select has value */
             allElementHasOnLoad.forEach((e) => {
                 const valueOnload = e.getAttribute('bys-value');
                 if (valueOnload) {
@@ -98,10 +75,18 @@ export default grapesjs.plugins.add('bys-command-custom-export-template', (edito
             });
             scriptTag.append('}\n);');
             headElement.append(scriptTag);
+            /* build send email modal with button has click event */
+            const hasSendEmail = allElement.querySelectorAll('button[onclick="openModalSendEmail()"]') || [];
+            if (hasSendEmail.length) {
+                const scriptSendEmail = createSendEmailModalScript();
+                const styleSendEmail = createSendEmailModalStyle();
+                const modalSendEmail = createModalSendEmail();
+                headElement.append(scriptSendEmail);
+                headElement.append(styleSendEmail);
+                bodyElement.append(modalSendEmail);
+            }
             htmlElement.append(headElement);
             bodyElement.append(allElement);
-            const modalSendEmail = createModalSendEmail();
-            bodyElement.append(modalSendEmail);
             htmlElement.append(bodyElement);
 
             this.htmlEditor.setContent(htmlElement.outerHTML);
@@ -149,105 +134,273 @@ export default grapesjs.plugins.add('bys-command-custom-export-template', (edito
 
     function createModalSendEmail() {
         const modalSendEmail = document.createElement('div');
-        modalSendEmail.innerHTML = `<div id="sendEmailModal" tabindex="-1" aria-labelledby="sendEmailModalLabel" aria-hidden="true" class="modal fade">
-        <div class="modal-dialog">
-            <form id="formSendEmail">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 id="sendEmailModalLabel" class="modal-title">Modal title
-                        </h5>
-                        <button type="button" data-bs-dismiss="modal" aria-label="Close" class="btn-close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-3">
-                                mailsettingid
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="mailsettingid">
-                            </div>
+        modalSendEmail.innerHTML = `<div id="sendEmailModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close" onclick="closeModalSendEmail()">&times;</span>
+                <p class="text-center">Send Email</p>
+            </div>
+            <div class="modal-body">
+                <form id="formSendEmail">
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            mailsettingid
                         </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                state
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="state">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                linkid
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="linkid">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                from
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="from">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                to
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="to">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                cc
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="cc">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                bcc
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="bcc">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                subject
-                            </div>
-                            <div class="col-9">
-                                <input class="form-control" name="subject">
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                body
-                            </div>
-                            <div class="col-9">
-                                <textarea class="form-control" name="body"></textarea>
-                            </div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-3">
-                                attachfiles
-                            </div>
-                            <div class="col-9">
-                                <input type="file" name="attachfiles">
-                            </div>
+                        <div class="w-75">
+                            <input class="form-control" name="mailsettingid">
                         </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" data-bs-dismiss="modal" class="btn btn-secondary">Close</button>
-                        <button onclick="sendEmail()" type="button" class="btn btn-primary">Send</button>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            state
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="state">
+                        </div>
                     </div>
-                </div>
-            </form>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            linkid
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="linkid">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            from
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="from">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            to
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="to">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            cc
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="cc">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            bcc
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="bcc">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            subject
+                        </div>
+                        <div class="w-75">
+                            <input class="form-control" name="subject">
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            body
+                        </div>
+                        <div class="w-75">
+                            <textarea rows="4" class="form-control" name="body"></textarea>
+                        </div>
+                    </div>
+                    <div class="d-flex mt-2">
+                        <div class="w-25">
+                            attachfiles
+                        </div>
+                        <div class="w-75">
+                            <input type="file" name="attachfiles">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2">
+                        <button type="button" onclick="closeModalSendEmail()">Close</button>
+                        <button type="button" class="ms-2" onclick="sendEmail()">Send</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>`;
         return modalSendEmail;
+    }
+
+    function createDefaultStyle() {
+        const styleElement = document.createElement('style');
+        styleElement.append(`
+        /* Global style */
+        .d-flex {
+            display: flex;
+        }
+
+        .w-25 {
+            width: 25%;
+        }
+
+        .w-50 {
+            width: 50%;
+        }
+
+        .w-75 {
+            width: 75%;
+        }
+
+        .w-100 {
+            width: 100%;
+        }
+
+        .form-control {
+            width: 100%;
+            border: 1px solid gray;
+            border-radius: 0.25rem;
+            height: 2rem;
+            padding: 0.5rem;
+        }
+
+        .mt-2 {
+            margin-top: 0.5rem;
+        }
+
+        .justify-content-center {
+            justify-content: center;
+        }
+
+        .ms-2 {
+            margin-left: 0.5rem;
+        }
+
+        .text-center {
+            text-align: center;
+        }`);
+        return styleElement;
+    }
+
+    function createSendEmailModalStyle() {
+        const styleElement = document.createElement('style');
+        styleElement.append(`
+        /* The Modal (background) */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0, 0, 0);
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        /* Modal Content */
+        .modal-content {
+            position: relative;
+            background-color: #fefefe;
+            margin: auto;
+            padding: 0;
+            width: 40%;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            -webkit-animation-name: animatetop;
+            -webkit-animation-duration: 0.4s;
+            animation-name: animatetop;
+            animation-duration: 0.4s
+        }
+
+        /* Add Animation */
+        @-webkit-keyframes animatetop {
+            from {
+                top: -300px;
+                opacity: 0
+            }
+
+            to {
+                top: 0;
+                opacity: 1
+            }
+        }
+
+        @keyframes animatetop {
+            from {
+                top: -300px;
+                opacity: 0
+            }
+
+            to {
+                top: 0;
+                opacity: 1
+            }
+        }
+
+        /* The Close Button */
+        .close {
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+            position: absolute;
+            right: 1rem;
+            top: 0.5rem;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        .modal-header {
+            padding: 1rem;
+            border-bottom: 1px solid lightgray;
+        }
+
+        .modal-header p {
+            margin: 0;
+        }
+
+        .modal-body {
+            padding: 2px 16px;
+        }`);
+        return styleElement;
+    }
+
+    function createSendEmailModalScript() {
+        const scriptElement = document.createElement('script');
+        scriptElement.append(`var modalSendEmail;
+        window.addEventListener('DOMContentLoaded', function () {
+            modalSendEmail = document.getElementById("sendEmailModal");
+            window.onclick = function (event) {
+                if (event.target == modalSendEmail) {
+                    closeModalSendEmail();
+                }
+            }
+        });
+        function closeModalSendEmail() {
+            modalSendEmail.style.display = "none";
+        }
+
+        function openModalSendEmail() {
+            modalSendEmail.style.display = "block";
+        }
+
+        function sendEmail() {
+            const formSendEmail = document.getElementById('formSendEmail');
+            const formSendEmailData = new FormData(formSendEmail);
+            for (var pair of formSendEmailData.entries()) {
+                console.log(pair[0] + ', ' + pair[1]);
+            }
+            window.parent.Call_API_SendMail(formSendEmailData);
+            closeModalSendEmail();
+            formSendEmail.reset();
+        }`);
+        return scriptElement;
     }
 })
